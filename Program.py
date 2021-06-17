f9c29c3fd6cfa176fcf11cb042c745149041fd38
Douglas Freitas <freitasd345@gmail.com>
2021-06-17 20:39:04 -0300

    Implementação da função de transferencia e correção do README

--- Program.py
+++ Program.py
@@ -1,7 +1,16 @@
 from os import close
-from tkinter import Event
 import PySimpleGUI as sg
-from PySimpleGUI.PySimpleGUI import Button, FileBrowse, FolderBrowse
+from PySimpleGUI.PySimpleGUI import Button, FileBrowse, FolderBrowse, WINDOW_CLOSED
+import shutil, errno
+    
+def Mover(src, dst):
+    try:
+        shutil.move(src, dst)
+    except OSError as e:
+        if e.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else:
+            print('Directory not copied. Error: %s' % e) 
 
 class ScreenPython:
     def __init__(self):
@@ -11,29 +20,26 @@ class ScreenPython:
              sg.InputText('Selecione o arquivo'), sg.FileBrowse()],
 
             [sg.Text('Diretorio de Origem', auto_size_text=True, justification='rigth'),      
-             sg.InputText('Selecione um diretorio', key='dir_1'), sg.FolderBrowse()],
+             sg.InputText('Selecione um diretorio', key='src'), sg.FolderBrowse()],
 
-            [sg.Text('Diretorio de Destino', key='dir_2', auto_size_text=True, justification='rigth'),
+            [sg.Text('Diretorio de Destino', key='dst', auto_size_text=True, justification='rigth'),
             sg.InputText('Selecionar diretorio'), sg.FolderBrowse()],
 
             [sg.Button('Start', key='start'),sg.Button('Closed', key='closed')]
         ]
         #Janela
-        self.janela = sg.Window("Transfere.ae!").layout(layout)
-        
+        self.janela = sg.Window("Transfere.ae!", layout)
 
-
-
-    #Extrair os dados da tela
+        #Extrair os dados da tela
     def Iniciar(self):
-        while True:    
-            self.button, self.values = self.janela.Read()
-            arquivo = self.values['arquivo']
-            dir_1 = self.values['dir_1']
-            dir_2 = self.values['dir_2']
+        while True: 
+            evento, values = self.janela.read()
+            if evento == WINDOW_CLOSED:
+                break
+            if evento == 'start':
+                Mover(values[0], values[1])
+                print("Arquivos foram transferidos")
             
-            print(self.values)
-    
 tela = ScreenPython()
 tela.Iniciar()
         
--- README.txt
+++ README.txt
@@ -1,14 +1,23 @@
-Realizar a instalação da utlima versão do Python 3.9.5
+Bem-vindo ao meu repositório
+
+Realizar a instalação da última versão do Python 3.9.5
 link(https://www.python.org/downloads/)
 
+Primeiro Passo
 Verificar se foi instalado corretamente
 python --version
-Na tela deverá aparecer 
-Python 3.9.5
+Na tela deverá aparecer
+Python 3.9.5 
+ 
 
-Configurar o interpretador do python no VSCode
-Ir até a parte de Extensions, pesquisar Python, selecionar a primeira opção e instalar
-
-Instalar a extensão, se possivel rodar ela no terminal do VScode  
+Segundo Passo
+Instalar a extensão do PySimpleGUI, abra o cmd ou Shell e digite o seguinte comando
 pip install pysimplegui
+Aguarde a instalação ser concluída e feche o terminal
 
+Terceiro Passo
+Configurar o interpretador do python no Visual Studio Code
+Ir até a parte de Extensions, pesquisar Python, selecionar a primeira opção e instalar.
+ 
+
+Configuração finalizada.
